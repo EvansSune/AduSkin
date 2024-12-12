b2159ed4fea9d177dc75bcf8e60f2059abddca3a
EvansSune <futuresun9672@163.com>
2024-12-12 10:37:20 +0800

    Add files via upload

---
+++ MainWindowViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Prism.Mvvm;
+using Prism.Regions;
+using PrismRegionTest.Views;
+
+namespace PrismRegionTest.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "Prism Application";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+        private IRegionManager _regionManager;
+        public MainWindowViewModel(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+            _regionManager.RegisterViewWithRegion("HeadRegion", typeof(HeadView));
+            _regionManager.RegisterViewWithRegion("LeftRegion", typeof(LeftView));
+            _regionManager.RegisterViewWithRegion("ContentRegion", typeof(ContentView));
+            _regionManager.RegisterViewWithRegion("StackPanelRegion", typeof(LeftView));
+
+            
+            //var LRegion = _regionManager.Regions["LeftRegion"]; // 对区域的访问
+            
+
+        }
+    }
+}
